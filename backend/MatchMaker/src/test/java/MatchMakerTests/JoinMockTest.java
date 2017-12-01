7f716f1300a1c3a8c30a552bc5cca861eb65161b
Unknown <pomogaibo.g.s@gmail.com>
2017-12-01 14:20:08 +0300

    JoinMockTest

--- backend/MatchMaker/src/test/java/MatchMakerTests/JoinMockTest.java
+++ backend/MatchMaker/src/test/java/MatchMakerTests/JoinMockTest.java
@@ -1,13 +1,25 @@
 package MatchMakerTests;
 
 
+import boot.MMController;
+import boot.MmApplication;
+import boot.Requests;
+import okhttp3.Response;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.BootstrapWith;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.io.IOException;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -15,15 +27,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @WebMvcTest
+@ContextConfiguration(classes = MmApplication.class)
 public class JoinMockTest {
     @Autowired
     MockMvc mockMvc;
-
     @Test
-    public void joinRequest() throws Exception {
+    public void join() throws Exception {
         mockMvc.perform(post("/matchmaker/join")
-                .content("name=Дядя_Моня_нагибатор")
+                .content("name=bom")
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE))
+                .andExpect(status().isOk())
                 .andExpect(content().string("42"));
     }
 
